b5dd5e43f000c75c365b7975d0b95e4fac3628d5
isabelgonz20 <36580465+isabelgonz20@users.noreply.github.com>
2019-08-31 00:09:55 -0600

    Add files via upload

--- Nodo.cpp
+++ Nodo.cpp
@@ -16,3 +16,30 @@ Nodo::~Nodo() {
 	// TODO Auto-generated destructor stub
 }
 
+void Nodo::agregarHijo(Nodo* nodo) {
+	hijos++;
+	if(primerHijo == NULL){
+		primerHijo = nodo;
+		ultimoHijo = nodo;
+	}else {
+		ultimoHijo -> siguiente = nodo;
+		ultimoHijo = nodo;
+	}
+
+}
+
+Nodo* Nodo::getHijos() {
+	return primerHijo;
+}
+
+Nodo::Nodo(string nombre) {
+	this -> nombre = nombre;
+	siguiente = NULL;
+	primerHijo = NULL;
+	ultimoHijo = NULL;
+	hijos = 0;
+}
+
+int Nodo::getNumeroHijos() {
+	return this -> hijos;
+}
--- Nodo.h
+++ Nodo.h
@@ -1,16 +1,50 @@
+/*
+ * Nodo.h
+ *
+ *  Created on: 30 ago. 2019
+ *      Author: Aisab
+ */
+
 #ifndef NODO_H_
 #define NODO_H_
-#include <String.h>
+#include <string>
 using namespace std;
 
 class Nodo {
 private:
 	Nodo* siguiente;
-	String nombre;
+	string nombre;
+	int hijos;
+	Nodo* primerHijo;
+	Nodo* ultimoHijo;
 public:
+	void agregarHijo(Nodo* nodo);
+	Nodo* getHijos();
+	Nodo(string nombre);
 
 	Nodo();
 	virtual ~Nodo();
+
+	void setHijos(int hijos) {
+		this->hijos = hijos;
+	}
+	int getNumeroHijos();
+
+	const string& getNombre() const {
+		return nombre;
+	}
+
+	void setNombre(const string& nombre) {
+		this->nombre = nombre;
+	}
+
+	const Nodo*& getSiguiente() const {
+		return siguiente;
+	}
+
+	void setSiguiente(const Nodo *&siguiente) {
+		this->siguiente = siguiente;
+	}
 };
 
 #endif /* NODO_H_ */
